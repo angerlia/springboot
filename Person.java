73afbafc1a8d062bf7fab9d4c8e70ae1248c0504
angerlia <2394001586@qq.com>
2018-09-15 10:44:57 +0800

    create project

--- Person.java
+++ Person.java
@@ -1,41 +1,41 @@
-//package com.anger.test_monday;
-//
-//import java.math.BigDecimal;
-//
-//public class Person {
-//
-//    private String name;
-//    private int qty;
-//
-//
-//    public String getName() {
-//        return name;
-//    }
-//
-//    public void setName(String name) {
-//        this.name = name;
-//    }
-//
-//    public int getQty() {
-//        return qty;
-//    }
-//
-//    public void setQty(int qty) {
-//        this.qty = qty;
-//    }
-//
-//
-//
-//    @Override
-//    public String toString() {
-//        return "Person{" +
-//                "name=" + name +
-//                ", age=" + qty +
-//                '}';
-//    }
-//    public Person(String name, int qty, BigDecimal price){
-//        this.setName(name);
-//        this.setQty(qty);
-//    }
-//
-//}
+package com.anger.test_monday;
+
+import java.math.BigDecimal;
+
+public class Person {
+
+    private String name;
+    private int qty;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name=" + name +
+                ", age=" + qty +
+                '}';
+    }
+    public Person(String name, int qty, BigDecimal price){
+        this.setName(name);
+        this.setQty(qty);
+    }
+
+}
